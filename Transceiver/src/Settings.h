9af0a1f51d7c4f6e84c94c08a34c9c8c02a9dba2
Maksim Savich <savichm2@msu.edu>
2025-01-05 20:36:39 -0600

    Coverted project to PlatformIO

---
+++ Transceiver/src/Settings.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <LittleFS.h>
+#include <RadioLib.h>
+#include "pb.h"
+#include "pb_encode.h"
+#include "pb_decode.h"
+#include "settings.pb.h"
+
+#define CONFIG_RADIO_FREQ           915.0
+#define CONFIG_RADIO_OUTPUT_POWER   22
+#define CONFIG_RADIO_BW             500.0
+#define CONFIG_RADIO_SF             7
+#define CONFIG_RADIO_CR             5
+#define CONFIG_RADIO_PREAMBLE       8
+#define CONFIG_RADIO_SETCRC         true
+#define CONFIG_RADIO_SW             0xAB
+#define CONFIG_RADIO_SETTINGS_FILE  "/settings.bin"
+
+void initializeSettings(Settings& settings, SX1262& radio);
+void loadSettingsFromFile(Settings& settings);
+void saveSettingsToFile(const Settings& settings);
+void configureLoRaSettings(const Settings& settings, SX1262& radio);
+void readSettings();
